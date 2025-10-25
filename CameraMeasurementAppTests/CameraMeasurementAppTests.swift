4e0023bbc15975651fde66769236d0df40a63181
chung <chungyueh@wuchongyuedeMacBook-Pro.local>
2025-10-25 12:29:51 +0800

    Initial Commit

---
+++ CameraMeasurementAppTests/CameraMeasurementAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CameraMeasurementAppTests.swift
+//  CameraMeasurementAppTests
+//
+//  Created by 吳崇岳 on 2025/10/25.
+//
+
+import Testing
+@testable import CameraMeasurementApp
+
+struct CameraMeasurementAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
